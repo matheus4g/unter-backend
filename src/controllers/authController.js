d0d219711bf2b1fe5b9f89509372008a83623b66
matheus4g <54090298+matheus4g@users.noreply.github.com>
2019-08-14 02:34:30 -0300

    Update authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -38,7 +38,7 @@ router.post('/authenticate', async (req, res) => {
 
   const user = await User.findOne({ email }).select('+password')
 
-  if (!user) { return res.status(400).send({ error: 'User not found' }) }
+  if (!user) { return res.send({ error: 'User not found' }) }
 
   if (!await bcrypt.compare(password, user.password)) {
     return res.status(400).send({ error: 'Invalid password' })
